9724619fbffe71ae3bc712f01e89dc3d71835e89
Brendan Prednis <brendan.prednis@protonmail.com>
2021-11-12 12:56:22 -0800

    structure of app.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - struct that contains things such as ptrs to db connections
+type App struct{}
+
+// Run - set up our application
+func (app *App) Run() error {
+	fmt.Println("setting up app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go RES API")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up REST API")
+
+	}
 }
