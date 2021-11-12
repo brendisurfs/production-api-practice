1ef32c348d766b1afe64c4dd4e27a8693ed43f17
Brendan Prednis <brendan.prednis@protonmail.com>
2021-11-12 13:05:07 -0800

    added health check endpoint
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/brendisurfs/go-rest-api/internal/transport/http"
+)
 
 // App - struct that contains things such as ptrs to db connections
 type App struct{}
@@ -8,6 +13,14 @@ type App struct{}
 // Run - set up our application
 func (app *App) Run() error {
 	fmt.Println("setting up app")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("failed to set up server")
+		return err
+	}
 	return nil
 }
 
@@ -17,6 +30,5 @@ func main() {
 
 	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up REST API")
-
 	}
 }
